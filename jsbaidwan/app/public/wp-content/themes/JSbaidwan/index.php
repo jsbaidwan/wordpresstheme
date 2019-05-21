73ef658bf8444f5d0446d037188b0fb74dfe57ee
jsbaidwan <jsbaidwan5@gmail.com>
2019-05-22 03:49:26 +0530

    limit the display post to 3

--- jsbaidwan/app/public/wp-content/themes/JSbaidwan/index.php
+++ jsbaidwan/app/public/wp-content/themes/JSbaidwan/index.php
@@ -1,10 +1,17 @@
 <?php get_header(); ?>
 
+<?php
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 3,
+);
 
-<?php     // Display posts using loop
-  while(have_posts()) {
-      the_post();
-?>
+$blogposts = new WP_Query($args); // Deals with what type of query is required for the page.
+
+  while($blogposts->have_posts()) { //Dispaly the posts using loop
+    $blogposts->the_post();  // Here we are running these methods on $blogposts object
+ ?>
+
   <a href="<?php the_permalink(); ?>">
     <h3><?php the_title(); ?></h3>
   </a>
--- jsbaidwan/app/public/wp-content/themes/JSbaidwan/single.php
+++ jsbaidwan/app/public/wp-content/themes/JSbaidwan/single.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
 <?php 
+
   while(have_posts()) {
       the_post();
  ?>
+
   <h2><?php the_title();?></h2>
-  Posted By <?php the_author();?>
+  Posted By <?php the_author(); ?> <br>
   <img src="<?php echo get_the_post_thumbnail_url(get_the_ID());?>"/>
   <?php the_content();
   comment_form();
